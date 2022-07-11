c4a57b328149b28feca3d85788cd56ff45e9f270
vominhtrungpro <68464213+vominhtrungpro@users.noreply.github.com>
2022-07-11 15:39:41 +0700

    Revert "Add files via upload"

--- Demo/js/demo.js
+++ Demo/js/demo.js
@@ -1,10 +1,12 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-toggle')) {
+    if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
@@ -15,33 +17,3 @@ function myFunction() {
       }
     }
   }
-
-  function myFunction2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-toggle')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
-window.onscroll = function() {myFunction3()};
-
-function myFunction3() {
-  if (document.documentElement.scrollTop > 1700) {
-    document.getElementById("myP").style.position = "absolute";
-    document.getElementById("myP").style.top = "1700px";
-  } else if(document.documentElement.scrollTop <= 1700) {
-    document.getElementById("myP").style.position = "fixed";
-    document.getElementById("myP").style.top = "70px";
-  }
-}
